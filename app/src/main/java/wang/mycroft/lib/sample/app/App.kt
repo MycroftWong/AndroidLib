0094dbe007f6201e7c0915535ac70c401665dbbf
MycroftWong <mycroftwong@gmail.com>
2019-11-13 15:40:40 +0800

    添加哆啦A梦测试工具

--- app/src/main/java/wang/mycroft/lib/sample/app/App.kt
+++ app/src/main/java/wang/mycroft/lib/sample/app/App.kt
@@ -3,7 +3,9 @@ package wang.mycroft.lib.sample.app
 import android.app.Application
 import com.blankj.utilcode.util.LogUtils
 import com.blankj.utilcode.util.Utils
+import com.didichuxing.doraemonkit.DoraemonKit
 import com.tencent.smtt.sdk.QbSdk
+import wang.mycroft.lib.sample.ui.activity.ArticleWebViewActivity
 import wang.mycroft.lib.sample.ui.view.SpinLoadingAdapter
 import wang.mycroft.lib.view.Loading
 
@@ -25,5 +27,11 @@ class App : Application() {
         LogUtils.getConfig().isLogSwitch = true
 
         QbSdk.initX5Environment(this, null)
+
+        DoraemonKit.install(this)
+
+        DoraemonKit.setWebDoorCallback { context, url ->
+            context.startActivity(ArticleWebViewActivity.getIntent(context, "哆啦A梦", url))
+        }
     }
 }
